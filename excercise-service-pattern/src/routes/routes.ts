f50160cf545b60dc41f79839cad87711a5f7a428
mahdi215 <63904571+mahdi215@users.noreply.github.com>
2024-12-18 20:57:26 +0700

    Add files via upload

---
+++ excercise-service-pattern/src/routes/routes.ts
@@ -0,0 +1,10 @@
+import categoriesController from "../models/categories.controller";
+import router from "./api";
+
+
+// CRUD Categories
+router.get("/categories", categoriesController.findAll);
+router.post("/categories", categoriesController.create);
+router.get("/categories/:id", categoriesController.findOne);
+router.put("/categories/:id", categoriesController.update);
+router.delete("/categories/:id", categoriesController.delete);
